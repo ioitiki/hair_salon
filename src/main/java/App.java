937836d79777f8e35083e94f4bb1102fe0676e45
Andrew Malkin <andymalkin@hotmail.com>
2017-03-31 16:41:40 -0700

    add about page and clean up some styling

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -73,6 +73,13 @@ public class App {
       model.put("template", "templates/client.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-    
+
+    get("/about", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/about.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
