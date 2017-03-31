1bfe3e835f854a7ffb339c0201a0639b0b00163c
Andrew Malkin <andymalkin@hotmail.com>
2017-03-31 16:00:09 -0700

    add stylist page with button to add client as well as the form to add client

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -32,5 +32,41 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/stylists/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      model.put("stylist", stylist);
+      model.put("clients", stylist.getClients());
+      model.put("template", "templates/stylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists/:id/clients/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      model.put("stylist", stylist);
+      model.put("clients", stylist.getClients());
+      model.put("template", "templates/client-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stylists/:id/clients/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params(":id"));
+      String name = request.queryParams("name");
+      String description = request.queryParams("description");
+      int age = Integer.parseInt(request.queryParams("age"));
+      String gender = request.queryParams("gender");
+      Client newClient = new Client(name, id, description, age, gender);
+      newClient.save();
+      String url = "/stylists/" + id;
+      response.redirect(url);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+
+
+
   }
 }
