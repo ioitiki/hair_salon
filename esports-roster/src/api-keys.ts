e8c44a87a5058a52d1c2cbb0212cfe294ff9c7cd
Andrew Malkin <andymalkin@hotmail.com>
2017-05-13 14:30:49 -0700

    create app.routing and the about component with a link in the nav bar

---
+++ esports-roster/src/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyA4vB9lwvbhz0EZuN_wCNBDJP1rJzfplos",
+  authDomain: "esports-roster.firebaseapp.com",
+  databaseURL: "https://esports-roster.firebaseio.com",
+  projectId: "esports-roster",
+  storageBucket: "esports-roster.appspot.com",
+  messagingSenderId: "762551442094"
+};
--- esports-roster/src/app/app.module.ts
+++ esports-roster/src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterializeModule } from 'angular2-materialize';
 
+import { routing } from './app.routing';
+import { masterFirebaseConfig } from './../api-keys';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
-    MaterializeModule
+    MaterializeModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
